341e037418ccc25f872fcb0474389e9b70ca7e2b
sjkfeel <layum@naver.com>
2025-02-12 14:32:32 +0900

    오늘

---
+++ first/index.js
@@ -0,0 +1,10 @@
+// 문자열 글자 '' "" ``
+const myString = "김석진";
+
+const body = document.querySelector("body");
+const p = document.createElement("p");
+
+p.innerText = myString;
+p.style.color = "blue";
+p.className = "header";
+body.append(p);
